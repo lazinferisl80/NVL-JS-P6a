826293b0b101084e5fe93525f8672888f72ca03a
IvanSL80 <isl.ivan.ivan@gmail.com>
2019-11-20 19:49:28 +0100

    ok

---
+++ app.js
@@ -0,0 +1,29 @@
+var nombredelusuario="Ivan"; 
+
+console.log(nombredelusuario)
+
+let edaddelusuario="39 años";
+
+console.log(edaddelusuario)
+
+diasdelasemana="lunes,martes,miercoles,jueves,viernes,sabado y domingo"
+
+console.log(diasdelasemana)
+ 
+lagravedad="9,8";
+
+console.log(lagravedad)
+
+primerosnumeros="1,3,5";
+
+console.log(primerosnumeros)
+
+var numero = 200;
+
+console.log(numero)
+var nombre="ivan";
+var apellido="sanchez lopez";
+
+var nombrecompleto= nombre + " " + apellido;
+
+console.log(nombrecompleto)
